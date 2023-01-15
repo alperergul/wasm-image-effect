e227edf2859200ca6723c1b81743aa25e11854cb
alperergul <alper.ergul.97@gmail.com>
2023-01-16 00:57:00 +0300

    initial

---
+++ public/main.js
@@ -0,0 +1,32 @@
+async function init() {
+  let rustApp = null;
+
+  try {
+    rustApp = await import("../pkg");
+  } catch (e) {
+    console.log(e);
+    return;
+  }
+
+  console.log(rustApp);
+
+  const input = document.getElementById("upload");
+  const fileReader = new FileReader();
+
+  fileReader.onloadend = () => {
+    let base64 = fileReader.result.replace(
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+
+    let image_data_url = rustApp.grayscale(base64);
+    console.log(image_data_url);
+    document.getElementById("new-img").setAttribute("src", image_data_url);
+  };
+
+  input.addEventListener("change", () => {
+    fileReader.readAsDataURL(input.files[0]);
+  });
+}
+
+init();
